5fd83edf296c34319c24ecab7a6a21dfaaf07c78
giustoantonadev <giustoantonadev@gmail.com>
2026-02-20 14:00:50 +0100

    snack-3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const numbersUp = []
+
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+    numbersUp.push(number + 1)
+}
+
+console.log(numbersUp);
