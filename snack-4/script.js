57ad922ee0ad307d66bfaf0c07e8cc8a131dab60
giustoantonadev <giustoantonadev@gmail.com>
2026-02-20 14:03:58 +0100

    snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,7 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authorsName = posts.map(authors => authors.author);
+
+console.log(authorsName);
